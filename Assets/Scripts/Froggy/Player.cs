a66e31cf6a00bd7b36b3e9ab270070961c90b97c
Jimmie-gh <Jimmie-gh@users.noreply.github.com>
2020-11-01 16:33:12 +0200

    init

---
+++ Assets/Scripts/Froggy/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    public float speed = 3.14f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float froggyHAxis = Input.GetAxis("Horizontal");
+      if (froggyHAxis != 0)
+        {
+            transform.position = new Vector3((transform.position.x+ froggyHAxis*Time.deltaTime*speed), transform.position.y);
+        }
+    }
+}
